0c24f5a2b398533938089837a074895cd93a5492
kenpeter <figo2476@hotmail.com>
2017-04-12 16:48:32 +1000

    update

--- src/api/car.js
+++ src/api/car.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 export default {
   // cb is used to dispatch...
   getCars (cb) {
-    axios.get('http://driveback.shopshop.space/car/list')
+    axios.get('http://driveback.shopshop.space/car/list?limit=100')
       .then((res) => {
         cb(res.data)
       })
